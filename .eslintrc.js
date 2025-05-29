7395c3e1ad8e7d48e3f6c87bc02125cfc3e276e3
Galahad-Zero <galahad0425@gmail.com>
2025-05-29 11:29:31 +0800

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: null,
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', {
+      varsIgnorePattern: '^_',
+      argsIgnorePattern: '^_'
+    }],
+    'no-console': 'off',
+    'prettier/prettier': 'error',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single']
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'webpack.config.js']
+}; 
